b567daecf3c354e1f700017f1bc482ab134280b6
Danlowlows <37116532+Danlowlows@users.noreply.github.com>
2019-11-16 17:40:23 -0800

    Create GeneratePyfbsdkImports.py
    Added new script that searches through a given Motionbuilder Python file and generates a list of all FB functions and classes for you to copy and paste, as a replacement for " from pyfbsdk import * ".

---
+++ MobuCore/MobuCoreTools/GeneratePyfbsdkImports/GeneratePyfbsdkImports.py
@@ -0,0 +1,21 @@
+def ReadFileToList(filePath):
+    with open(filePath) as f:
+        lineList = f.readlines()
+    return lineList
+
+def GeneratePyfbsdkImports(pythonFilePath):
+    lines = ReadFileToList(pythonFilePath)
+    fbNames = []
+    for line in lines:
+        if " " in line:
+            lineParts = line.split(" ")
+            for part in lineParts:
+                if part.startswith("FB"):
+                    fbNames.append(part)   
+    fbNames = [name.split("(")[0] for name in fbNames]
+    fbNames = [name.split(".")[0] for name in fbNames]
+    fbNames = [name.strip('\n') for name in fbNames]
+    fbNames = [name.strip('\,') for name in fbNames]
+    fbNames.remove("FBDelete")
+    fbNames = list(dict.fromkeys(fbNames))
+    print str(fbNames).replace("[","").replace("]","").replace("'","")
